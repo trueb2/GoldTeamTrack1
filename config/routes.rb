dbf96c799eca7e39ed8d68e6949a918b17ae32bc
Sam Okrent <samuelokrent@gmail.com>
2017-11-13 15:18:29 -0600

    Home route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :companies
   resources :facilities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
 end
